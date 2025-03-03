a3b7fff1eb48d888da0f756927eaf28f81b54900
Parisi Fabio <parisifabio98@gmail.com>
2025-03-03 21:29:18 +0100

    resolve button problem

--- Player/ray_cast_3d.gd
+++ Player/ray_cast_3d.gd
@@ -4,6 +4,7 @@ func _ready() -> void:
 	label.text= ""
 func _process(delta: float) -> void:
 	if is_colliding():
+		print('aaaaaaaaaaaaaaaaaa')
 		var collider = get_collider()
 		if collider is interactible:
 			label.text = collider.int_name
